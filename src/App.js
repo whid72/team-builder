8b1071b201da1d817bd0b98bf587734cade26152
Darrion_White <wdarrion64@gmail.com>
2021-12-14 17:25:47 -0600

    Basic setup finalized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect }  from 'react';
+
 import './App.css';
 
+const initialFormValues = {
+  ///// TEXT INPUTS /////
+  username: '',
+  email: '',
+  ///// DROPDOWN /////
+  role: '',
+}
+
 function App() {
+  const [member, setMember] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues); // fix this using the state hook
+  const [error, setError] = useState("");
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+    </>
   );
 }
 
